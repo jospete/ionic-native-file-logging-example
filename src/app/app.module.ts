a2a7b7f426c50993ee491afdd8ccac2467b093a3
= <obsidize@gmail.com>
2023-02-10 15:18:07 -0600

    migrate ionic-native -> awesome-cordova-plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { File as CordovaFile } from '@ionic-native/file/ngx';
-import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { File as CordovaFile } from '@awesome-cordova-plugins/file/ngx';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   imports: [
-    BrowserModule, IonicModule.forRoot(),
+    BrowserModule,
+    IonicModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
