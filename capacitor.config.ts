a71a0e8bca0523a86b08816153c78b211ea891f4
Josh Noel <obsidize@gmail.com>
2023-11-28 13:14:42 -0600

    misc updates for android build

---
+++ capacitor.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="@capacitor/splash-screen" />
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-testapp',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorCookies: {
+      enabled: true,
+    },
+    CapacitorHttp: {
+      enabled: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 0
+    },
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#CE0B7C',
+    },
+    PushNotifications: {
+      presentationOptions: ["alert", "sound"]
+    }
+  },
+};
+
+export default config;
