95b056a9b94e8c721dce3bd0c402e557847ec79c
Josh Noel <obsidize@gmail.com>
2023-11-29 03:41:50 -0600

    update to latest secure logger plugin version

--- src/app/services/log-manager.service.ts
+++ src/app/services/log-manager.service.ts
@@ -4,7 +4,7 @@ import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 import { Logger, LogLevel, getPrimaryLoggerTransport } from '@obsidize/rx-console';
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 import { environment } from 'src/environments/environment';
-import { SecureLogger } from 'cordova-plugin-secure-logger';
+import { SecureLogger, sendRxConsoleEventToNative } from 'cordova-plugin-secure-logger';
 
 const primaryTransport = getPrimaryLoggerTransport();
 const targetLogLevel = environment.production ? LogLevel.DEBUG : LogLevel.VERBOSE;
@@ -14,7 +14,7 @@ primaryTransport
   .setFilter(ev => ev.level >= targetLogLevel)
   .setDefaultBroadcastEnabled(!environment.production)
   .events()
-  .addListener(SecureLogger.webviewEventListenerProxy);
+  .addListener(sendRxConsoleEventToNative);
 
 @Injectable({
   providedIn: 'root'
@@ -40,7 +40,7 @@ export class LogManagerService {
 
     if (!this.isNativePlatform) {
       this.logger.info(`removing native proxy (non-native-platform)`);
-      primaryTransport.events().removeListener(SecureLogger.webviewEventListenerProxy);
+      primaryTransport.events().removeListener(sendRxConsoleEventToNative);
       SecureLogger.clearEventCacheFlushInterval();
     }
   }
